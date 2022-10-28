bf74d7f58a3c131f1c7512d05552d5236fa5c274
Neeshka76 <83765170+Neeshka76@users.noreply.github.com>
2022-10-28 15:28:26 +0200

    Add project files.

---
+++ ImbuedOnSpawnLevelModule.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace ImbuedOnSpawn
+{
+    public class ImbuedOnSpawnLevelModule : LevelModule
+    {
+        public bool keepImbued = true;
+        public string[] imbueType =
+        {
+            "Fire",
+            "Lightning",
+            "Gravity"
+        };
+        public bool useWhitelistInstead = false;
+        public string[] blacklistItemsId =
+        {
+            ""
+        };
+        public string[] whitelistItemsId =
+        {
+            ""
+        };
+        
+
+        public override IEnumerator OnLoadCoroutine()
+        {
+            EventManager.onItemSpawn += EventManager_onItemSpawn;
+            return base.OnLoadCoroutine();
+        }
+
+        private void EventManager_onItemSpawn(Item item)
+        {
+            if (IsToImbued(item))
+                GiveRandomImbueEffect(item);
+        }
+
+        /// <summary>
+        /// Give the Random Imbue Effect to the Item
+        /// </summary>
+        private void GiveRandomImbueEffect(Item item)
+        {
+            string id = imbueType[UnityEngine.Random.Range(0, imbueType.Length)];
+            if (item.gameObject.GetComponent<RandomImbueEffect>() == null)
+                item.gameObject.AddComponent<RandomImbueEffect>().Init(item, id, keepImbued);
+            else
+            {
+                item.gameObject.GetComponent<RandomImbueEffect>().Dispose();
+                item.gameObject.AddComponent<RandomImbueEffect>().Init(item, id, keepImbued);
+            }
+        }
+
+        private bool IsToImbued(Item item)
+        {
+            if (!useWhitelistInstead)
+            {
+                if (!blacklistItemsId.Contains(item.itemId))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (whitelistItemsId.Contains(item.itemId))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public override void OnUnload()
+        {
+            base.OnUnload();
+            EventManager.onItemSpawn -= EventManager_onItemSpawn;
+        }
+    }
+}
---
+++ RandomImbueEffect.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace ImbuedOnSpawn
+{
+    public class RandomImbueEffect : MonoBehaviour
+    {
+        private string imbueInstance;
+        private Item item;
+        private bool keepImbued;
+
+        public void Init(Item item, string idImbue, bool keepImbued)
+        {
+            this.item = item;
+            imbueInstance = idImbue;
+            this.keepImbued = keepImbued;
+            this.item.OnDespawnEvent += Item_OnDespawnEvent;
+        }
+
+        private void Item_OnDespawnEvent(EventTime eventTime)
+        {
+            if (eventTime == EventTime.OnStart)
+                Dispose();
+        }
+
+        public void Update()
+        {
+            if (!keepImbued) return;
+            if (Snippet.imbueBelowLevelItem(item, 75))
+            {
+                Snippet.ImbueItem(item, imbueInstance);
+            }
+        }
+        public void Dispose()
+        {
+            item.OnDespawnEvent -= Item_OnDespawnEvent;
+            Destroy(this);
+        }
+    }
+}
